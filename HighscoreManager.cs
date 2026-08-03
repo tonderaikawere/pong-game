108fa89965274b6290bff8f6ee468d1d08b80d2a
TonderaiKawere <tondeskawere@gmail.com>
2026-08-03 12:45:00 +0200

    Declare score file destination constant

--- HighscoreManager.cs
+++ HighscoreManager.cs
@@ -4,5 +4,8 @@ using System.IO;
 namespace PongGame
 {
     public static class HighscoreManager
+    {
+        private const string FileName = "highscores.txt";
 
-// Commit step 55 of 150
+
+// Commit step 56 of 150
