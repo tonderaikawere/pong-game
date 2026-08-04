3b84699f547084148659c69f49ad8358223e0723
TonderaiKawere <tondeskawere@gmail.com>
2026-08-04 15:00:00 +0200

    Define audio helper static class

--- SoundGenerator.cs
+++ SoundGenerator.cs
@@ -6,5 +6,13 @@ namespace PongGame
     public static class SoundGenerator
     {
         public static void GenerateBeep(string filePath, double frequency, double durationSeconds)
+        {
+            int sampleRate = 22050;
+            short bitsPerSample = 16;
+            short channels = 1;
+            int numSamples = (int)(sampleRate * durationSeconds);
+            int dataLength = numSamples * channels * (bitsPerSample / 8);
+            
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
 
-// Commit step 69 of 150
+// Commit step 70 of 150
