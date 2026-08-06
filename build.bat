0546dae899e39049f67d88833954deef184cf7d0
TonderaiKawere <tondeskawere@gmail.com>
2026-08-06 14:15:00 +0200

    Add compilation errors check codes

--- build.bat
+++ build.bat
@@ -2,5 +2,7 @@
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /target:winexe /out:PongGame.exe Program.cs PongGame.cs SoundGenerator.cs HighscoreManager.cs
 if %errorlevel% neq 0 (
     echo Compilation Failed!
+    pause
+    exit /b %errorlevel%
 
-:: Commit step 90 of 150
+:: Commit step 91 of 150
