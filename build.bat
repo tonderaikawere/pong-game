232b96278989eb5c3c6cd02efaae50afad6cfa77
TonderaiKawere <tondeskawere@gmail.com>
2026-08-06 12:45:00 +0200

    Initialize command line build batch

---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /target:winexe /out:PongGame.exe Program.cs PongGame.cs SoundGenerator.cs HighscoreManager.cs
+
+:: Commit step 89 of 150
