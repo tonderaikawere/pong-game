1e17fe56f67eef64231f9b48bea62d43c69c2b79
TonderaiKawere <tondeskawere@gmail.com>
2026-08-07 10:30:00 +0200

    Declare PlayMode nested enumerations definitions

--- PongGame.cs
+++ PongGame.cs
@@ -45,5 +45,17 @@ namespace PongGame
         private const float LogicalWidth = 800f;
         private const float LogicalHeight = 600f;
         private const float PaddleWidth = 15f;
+        private const float PaddleHeight = 90f;
+        private const float BallSize = 12f;
 
-// Commit step 96 of 150
+        private System.Media.SoundPlayer soundHitPaddle;
+        private System.Media.SoundPlayer soundHitWall;
+        private System.Media.SoundPlayer soundScore;
+        private System.Media.SoundPlayer soundGameOver;
+
+        private const string WavHitPaddle = "hit_paddle.wav";
+        private const string WavHitWall = "hit_wall.wav";
+        private const string WavScore = "score.wav";
+        private const string WavGameOver = "game_over.wav";
+
+// Commit step 97 of 150
